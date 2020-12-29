cffdbd21c1dd5c56eff123a7941e9ad43a53550f
yuvarajan <yuvasyuvarajan@gmail.com>
2020-12-29 16:47:12 +0530

    added some functionality

--- main.py
+++ main.py
@@ -1,23 +1,39 @@
-from PIL import ImageGrab
+from PIL import ImageGrab,ImageSequence,Image
 import os
-import random
+import time
 
-#For naming convention random module is used
-rand=random.randint(10000,300000)
+
+
+
+
+path = '/home/sk84/PycharmProjects/scrrenshot_project/pics'
+global index
 
 def take_screenshot():
     """""
-    This method will take screenshot without the knowledge of user
-    and save it in a "pics" folder
+    This method will take screenshot and save it in a "pics" folder
     """""
-    image= ImageGrab.grab()
-    path='/home/sk84/PycharmProjects/scrrenshot_project/pics'
+    index=1
+
     try:
+
         os.mkdir(path)
-        image.save('/home/sk84/PycharmProjects/scrrenshot_project/pics/test'+str(rand)+'.jpg')
+        while True:
+            print("loop starts")
+            images = ImageGrab.grab()
+            print(images)
+            print("grabbed")
+            #for image in ImageSequence.Iterator(images):
+            images.save('/home/sk84/PycharmProjects/scrrenshot_project/pics/test' + str(index) + '.jpg')
+            index=index+1
+            print(index)
+            print("saved")
+            time.sleep(2)
+            print("sleeping 2 secs")
+
     except OSError:
         if os.path.exists(path):
-            image.save('/home/sk84/PycharmProjects/scrrenshot_project/pics/test' + str(rand) + '.jpg')
+            images.save('/home/sk84/PycharmProjects/scrrenshot_project/pics/test' + str(index) + '.jpg')
 
 
 if __name__ == '__main__':
