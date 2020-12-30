063d1b19ec50d8e0bf8cc42ff95c50ee407e22a8
yuvarajan <yuvasyuvarajan@gmail.com>
2020-12-30 11:47:46 +0530

    added uuid for naming

--- main.py
+++ main.py
@@ -1,39 +1,39 @@
-from PIL import ImageGrab,ImageSequence,Image
-import os
+from PIL import ImageGrab
 import time
+import os
+import uuid
+path = '/home/sk84/PycharmProjects/scrrenshot_project/pics/'
 
-
-
-
-
-path = '/home/sk84/PycharmProjects/scrrenshot_project/pics'
-global index
 
 def take_screenshot():
     """""
     This method will take screenshot and save it in a "pics" folder
     """""
-    index=1
 
+    global index, images
+
+    i=uuid.uuid4()
+    index=i.int
     try:
 
         os.mkdir(path)
+
         while True:
-            print("loop starts")
-            images = ImageGrab.grab()
-            print(images)
-            print("grabbed")
-            #for image in ImageSequence.Iterator(images):
-            images.save('/home/sk84/PycharmProjects/scrrenshot_project/pics/test' + str(index) + '.jpg')
-            index=index+1
-            print(index)
-            print("saved")
-            time.sleep(2)
-            print("sleeping 2 secs")
+
+                print("loop is running")
+                images = ImageGrab.grab()
+                images.save('/home/sk84/PycharmProjects/scrrenshot_project/pics/' + str(index)+'.jpg' )
+                index = index + 1
+                time.sleep(2)
 
     except OSError:
-        if os.path.exists(path):
-            images.save('/home/sk84/PycharmProjects/scrrenshot_project/pics/test' + str(index) + '.jpg')
+        while True:
+         if os.path.exists(path):
+             images = ImageGrab.grab()
+             images.save('/home/sk84/PycharmProjects/scrrenshot_project/pics/' + str(index) + '.jpg')
+             index = index + 2
+             print(index)
+             time.sleep(2)
 
 
 if __name__ == '__main__':
